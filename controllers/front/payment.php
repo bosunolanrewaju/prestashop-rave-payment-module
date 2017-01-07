3a65d20178e83cadcdc6369cbe5ac53cf4d4320e
bosunolanrewaju <bosunolanrewaju@gmail.com>
2017-01-07 19:29:30 +0100

    Check order currency

--- controllers/front/payment.php
+++ controllers/front/payment.php
@@ -14,7 +14,10 @@
       $this->display_column_left = false;
       $this->display_column_right = false;
       parent::initContent();
-      $this->setTemplate('payment.tpl');
+
+      if (!$this->checkCurrency())
+        Tools::redirect('index.php?controller=order');
+
       $this->context->smarty->assign(array(
         'nb_products' => $this->context->cart->nbProducts(),
         'cart_currency' => $this->context->cart->id_currency,
@@ -22,5 +25,24 @@
         'total_amount'=> $this->context->cart->getOrderTotal(true, Cart::BOTH),
         'path' => $this->module->getPathUri(),
       ));
+
+      $this->setTemplate('payment.tpl');
+    }
+
+    private function checkCurrency()
+    {
+
+      $currency_order = new Currency($this->context->cart->id_currency);
+      $currencies_modules = $this->module->getCurrency($this->context->cart->id_currency);
+
+      if (is_array($currencies_modules)) {
+        foreach ($currencies_module as $currency_module) {
+          if ($currency_order->id == $currency_module['id_currency']) {
+            return true;
+          }
+        }
+
+        return false;
+      }
     }
   }
